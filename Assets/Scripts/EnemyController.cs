c8057025ca4ec08b504ebeffc55adc0d9566dd7f
jack leavey <jrleavey@gmail.com>
2021-11-30 13:08:42 -0500

    Init Commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    [SerializeField]
+    public ParticleSystem smokeEffect;
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    bool broken = true;
+    Animator animator;
+    AudioSource audioSource;
+    private RubyController _player;
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<RubyController>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = changeTime;
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    void Update()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+        _player.UpdateFixed();
+    }
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class RubyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public GameObject projectilePrefab;
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+    public ParticleSystem DamageEffect;
+    public ParticleSystem HealEffect;
+    [SerializeField]
+    private bool _isGameOver = false;
+    [SerializeField]
+    private Text _restartLevelText;
+    [SerializeField]
+    private Text wintext;
+    [SerializeField]
+    private Text loseText;
+    [SerializeField]
+    private Text ammotext;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    public float timeInvincible = 2.0f;
+    [SerializeField]
+    bool isInvincible;
+    float invincibleTimer;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    AudioSource audiosource1;
+    [SerializeField]
+    AudioSource audiosource2;
+    public int FixedRobots;
+    public Text FixedText;
+    private int ammo = 4;
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        audioSource = GetComponent<AudioSource>();
+        _restartLevelText.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+            Movement();
+            if (Input.GetKeyDown(KeyCode.C) && ammo > 0)
+            {
+                Launch();
+                ammo--;
+                UpdateAmmo();
+            }
+        
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
+        {
+            SceneManager.GetActiveScene();
+        }
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.X) && _isGameOver == true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+            if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            animator.SetTrigger("Hit");
+            PlaySound(hitSound);
+            DamageEffect.Play();
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        else
+        {
+            HealEffect.Play();
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+        if (health <= 0)
+        {
+            _isGameOver = true;
+            GameOverSequence();
+        }
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
+        PlaySound(throwSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+    public void GameOverSequence()
+    {
+        AudioSource source1 = GameObject.FindGameObjectWithTag("Bmusic").GetComponent<AudioSource>();
+        source1.Stop();
+        if (currentHealth == 0)
+        {
+            loseText.gameObject.SetActive(true);
+            audiosource2.Play();
+        }
+        else
+        {
+            wintext.gameObject.SetActive(true);
+            audiosource1.Play();
+        }
+    }
+
+    void Movement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+   public void UpdateFixed()
+    {
+        FixedRobots = FixedRobots + 1;
+        FixedText.text = "Robots Fixed: " + FixedRobots.ToString();
+
+        if(FixedRobots >= 4)
+        {
+            _isGameOver = true;
+            GameOverSequence();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public void UpdateAmmo()
+    {
+        ammotext.text = "Cogs: " + ammo.ToString();
+    }
+    public void AddAmmo()
+    {
+        ammo = ammo + 4;
+        UpdateAmmo();
+    }
+}
+
+
